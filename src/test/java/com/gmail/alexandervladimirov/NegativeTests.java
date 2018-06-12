a4c74abace3681f664c609e000ea355aa6a6e588
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-06-12 12:03:47 +0300

    Negative Tests Class created.

--- src/test/java/com/gmail/alexandervladimirov/NegativeTests.java
+++ src/test/java/com/gmail/alexandervladimirov/NegativeTests.java
@@ -8,15 +8,15 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
- * Created on 11.06.18.
+ * Created on 12.06.18.
  *
  * @author Alexander Vladimirov
  * <alexandervladimirov1902@gmail.com>
  */
-public class PositiveTest {
+public class NegativeTests {
 
     @Test
-    public void login() {
+    public void invalidUser() {
         //Create Driver.
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
@@ -28,28 +28,20 @@ public class PositiveTest {
 
         //Enter username.
         WebElement username = driver.findElement(By.id("username"));
-        username.sendKeys("tomsmith");
+        username.sendKeys("ivalidUser");
         //Enter password.
         WebElement password = driver.findElement(By.id("password"));
         password.sendKeys("SuperSecretPassword!");
         // Push login button.
-        WebElement logingButton = driver.findElement(By.className("radius"));
-        logingButton.click();
-        //Verification
-        //new URL
-        String expectedUrl = "http://the-internet.herokuapp.com/secure";
-        String actualUrl = driver.getCurrentUrl();
-        Assert.assertEquals(actualUrl, expectedUrl);
+        WebElement loginButton = driver.findElement(By.className("radius"));
+        loginButton.click();
 
-        //Logout out button is visible.
-        WebElement logoutButton = driver.findElement(By.xpath("//a[@class= 'button secondary radius']"));
-        Assert.assertTrue(logoutButton.isDisplayed(),"No logout is visible.");
-        // Success message.
-        WebElement successMessage = driver.findElement(By.id("flash"));
-        String expectedMessage = "You logged into a secure area!\n×";
-        String actualMessage  = successMessage.getText();
-        Assert.assertEquals(actualMessage,expectedMessage);
+        WebElement errorMessage = driver.findElement(By.xpath("//div[@class= 'flash error']"));
+        String expectedError = "Your username is invalid!\n×";
+        String actualError = errorMessage.getText();
+        Assert.assertEquals(actualError, expectedError);
         sleep(3000L);
+
         //Close Web Browser.
         driver.quit();
     }
